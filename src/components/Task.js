345d0b67703b0203261c478c5ce928295c14003b
jnfDev <jairo.jnf.2010@gmail.com>
2020-04-14 23:20:27 -0400

    Mejorar proceso para modificar tareas.

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,13 +4,23 @@ class Task extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            editing: false
+        }
+
         this.updateTask = this.updateTask.bind(this);
         this.deleteTask = this.deleteTask.bind(this);
+        this.editTask = this.setEditingState.bind(this);
     }
 
     updateTask(task, e){
         task.name = e.target.value;
         this.props.handler('update', task);
+    }
+
+    setEditingState(state) {
+        console.log('onBlur');
+        this.setState({ editing: state });
     }
 
     deleteTask(task, e) {
@@ -21,11 +31,17 @@ class Task extends React.Component {
         const task = this.props.task;
         return (
             <div id={task.id} className="task">
-                {task.name}
-                <input value={task.name} onChange={(e) => this.updateTask(task, e)}/>
+                {console.log(this.state.editing)}
+                {this.state.editing 
+                    ? <input value={task.name} onChange={(e) => this.updateTask(task, e)}/>
+                    : task.name
+                }
                 <div className="task-controls">
-                    <button>Create</button>
-                    <button>Edit</button>
+                    {this.state.editing
+                        ? <button onClick={(e) => this.setEditingState(false)}>Save</button>
+                        : <button>Add</button>
+                    }
+                    <button onClick={(e) => this.setEditingState(true)}>Edit</button>
                     <button onClick={(e) => this.deleteTask(task, e)}>Delete</button>
                 </div>
             </div>
